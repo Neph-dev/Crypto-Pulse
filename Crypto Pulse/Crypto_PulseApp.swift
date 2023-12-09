96c9ca41fcdada343dc9ddbc3626db8a32823783
Nephthali Salam <83629026+Neph-dev@users.noreply.github.com>
2023-12-09 18:37:22 +0200

    Initial Commit

---
+++ Crypto Pulse/Crypto_PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_PulseApp.swift
+//  Crypto Pulse
+//
+//  Created by Nephthali Salam on 2023/12/09.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
